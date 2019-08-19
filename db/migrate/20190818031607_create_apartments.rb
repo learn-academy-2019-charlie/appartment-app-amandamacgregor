af1d037213fa5b3521000427c922859d4a6c4f0b
Amanda <amanda.macgregor13@gmail.com>
2019-08-19 17:05:12 +0000

    working through walkthrough video

---
+++ db/migrate/20190818031607_create_apartments.rb
@@ -0,0 +1,10 @@
+class CreateApartments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :apartments do |t|
+      t.string :street
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
